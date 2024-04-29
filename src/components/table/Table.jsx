67024c6d1a0aa97ee7d52f9b676861ed1c5d93d9
beatrizac03 <beatrizaguiarc14@gmail.com>
2024-04-28 21:30:46 -0300

    react 1.2

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -2,11 +2,6 @@ import './Table.css'
 
 function Table( {participants} ) {
     let obj = participants
-    obj.forEach(obj => {
-        let each = document.createElement('div')
-        each.innerHTML = `${obj.name} + ${obj.email}`
-        console.log(each)
-    })
 
     return (
         <div className="wrapperTableCont">
